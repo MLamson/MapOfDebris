d7ed930db90bd23f1294f0dce165e2d30cda37fb
Mark Lamson <lamson_mark@yahoo.com>
2016-09-17 19:13:35 -0400

    MapTheDebris free code camp

--- spec/mapOfDebris.spec.ts
+++ spec/mapOfDebris.spec.ts
@@ -1,7 +1,10 @@
-import {testSetup} from "../src/mapOfDebris";
+import {MapOfDebris} from "../src/mapOfDebris";
 
-describe('test', () => {
-    it('true should be true', () => {
-        expect(testSetup.isWorking).toBe(true);
+describe('Map of Debris', () => {
+    it('given array with name and average altitude should return name orbital period', () => {
+        expect(MapOfDebris.toOrbitalPeriods([{name : "sputnik", avgAlt : 35873.5553}])).toEqual([{name: "sputnik", orbitalPeriod: 86400}]);
+    });
+    it('given array with two name and average altitude should return name and orbital period for both', () => {
+        expect(MapOfDebris.toOrbitalPeriods([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])).toEqual([{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}]);
     });
 });
--- src/mapOfDebris.ts
+++ src/mapOfDebris.ts
@@ -1,3 +1,28 @@
-export class testSetup {
-    static isWorking = true;
+export class MapOfDebris {
+
+    static toOrbitalPeriods(inputs: any): any {
+
+        let GM: number = 398600.4418;
+        let earthRadius: number = 6367.4447;
+
+        let nameAndOrbitalPeriods: any;
+        let orbitalPeriodCalc: number;
+
+        let finalListOfDebris: any = [];
+
+        for (let i: number = 0; i < inputs.length; i++) {
+
+            nameAndOrbitalPeriods = {};
+            orbitalPeriodCalc = 0;
+
+            orbitalPeriodCalc = Math.round( 2 * Math.PI * Math.sqrt(Math.pow((earthRadius + inputs[i].avgAlt), 3) / GM));
+
+            nameAndOrbitalPeriods = {name: inputs[i].name, orbitalPeriod: orbitalPeriodCalc};
+
+            finalListOfDebris.push(nameAndOrbitalPeriods);
+        }
+
+
+        return finalListOfDebris;
+    };
 }
